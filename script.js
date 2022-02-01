38d3a074d4cf1abcfaa87db3c4126c5d72f4bd89
daudi13 <davouma13@gmail.com>
2022-02-01 04:33:30 +0300

    Add + to replace Number object function

--- script.js
+++ script.js
@@ -4,17 +4,41 @@
 
 // Data
 const account1 = {
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    owner: 'David Ouma',
-    interestRate: 1.2, // %
-    pin: 1111,
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: 'David Ouma',
+  interestRate: 1.2, // %
+	pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
-    owner: 'Jessica Odhis',
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
+  owner: 'Jessica Odhis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -32,6 +56,8 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+
+console.log(account1)
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -128,7 +154,6 @@ const calcPrintBalance = function (acc) {
 }
 
 
-
 //display the incomes 
 
 const calcdisplaySummary = function (acc) {
@@ -169,7 +194,7 @@ btnLogin.addEventListener('click', (e) => {
 
 	console.log(currentAccount)
 
-	if (currentAccount?.pin === Number(inputLoginPin.value)) {
+	if (currentAccount?.pin === +(inputLoginPin.value)) {
 		console.log(`Hello Mr.${currentAccount.owner.split(' ')[1]}, welcome to SUbsidian Bank`);
 
 		// DISPLAY UI AND WELCOME MESSAGE
@@ -185,7 +210,7 @@ btnLogin.addEventListener('click', (e) => {
 
 btnTransfer.addEventListener('click', function (e) {
 	e.preventDefault();
-	const amount = Number(inputTransferAmount.value);
+	const amount = +(inputTransferAmount.value);
 	const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
 	inputTransferTo.value = inputTransferAmount.value = '';
 
@@ -202,7 +227,7 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnLoan.addEventListener('click', function (e) {
 	e.preventDefault()
-	const amount = Number(inputLoanAmount.value);
+	const amount = Math.floor(inputLoanAmount.value);
 	console.log(amount);
 
 	if (amount && amount > 0 && amount === currentAccount.movement.some(mov => mov >= amount * 0.1)) {
@@ -218,7 +243,7 @@ btnClose.addEventListener('click', function (e) {
 
     if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +(inputClosePin.value) === currentAccount.pin
     ) {
     const index = accounts.findIndex(
         acc => acc.username === currentAccount.username
@@ -254,5 +279,7 @@ console.log(movementsUi);
 labelBalance.addEventListener('click', () => {
 	const movementsUI = Array.from(document.querySelectorAll('.movements__value'), (el) => el.textContent.replace('$', ''));
 
-	const total = movementsUI.reduce((acc, cur) => acc + cur, 0)
+	const total = movementsUI.map(el => +(el)).reduce((cur, i) => cur + i);
+
+	console.log(total)
 })
